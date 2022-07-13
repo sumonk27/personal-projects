d23236b22a7d9170796005e05bb4ed21f54b8edf
Stephen Kamande <stevengudoc@gmail.com>
2022-07-13 21:11:23 +0300

    modified code to delete files and directories from temp files

--- cleanup.py
+++ cleanup.py
@@ -1,8 +1,23 @@
-import os
+import os,shutil
 
-path = "D:/New folder/"
-for file_name in os.listdir(path):
-    var = path + file_name
-    if os.path.isfile(var):
-        print('Deleting file: ', var)
-        os.remove(var)
+def file_del(path):
+    for file_name in os.listdir(path):
+        var = path + file_name
+        if os.path.isfile(var):
+            print('Deleting file: ', var)
+            try:
+                os.remove(var)
+            except PermissionError:
+                print('Unable to delete file: ', var)
+                pass
+        elif os.path.isdir(var):
+            print('Deleting dir: ', var)
+            try:
+                shutil.rmtree(var)
+            except PermissionError:
+                print('Unable to delete dir: ', var)
+                pass
+            
+file_del("C:/Windows/Temp/")
+file_del("C:/Windows/Prefetch/")
+file_del("C:/Users/steve/AppData/Local/Temp/")
